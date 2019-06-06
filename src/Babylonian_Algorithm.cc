29768730d2c7c4b9d695f52cf44d59e818293ac8
durdyGroBot <jacobWcorbin@gmail.com>
2019-06-05 17:25:50 -0700

    ABSOLUTE CPP PROJECT 209/314 UPDATED TO BE USED AS FUNCTION

--- src/Babylonian_Algorithm.cc
+++ src/Babylonian_Algorithm.cc
@@ -18,37 +18,35 @@ using std::cin;
 using std::endl;
 using std::ios;
 
-int main(int argc, char *argv[]){
+double babylonian_Algorithm(double n);
 
+int main(int argc, char *argv[]) {
     cout << "\nThe Babylonian Algorithm computes an estimate of the square root of a positive number n.\n";
-
-    cout << "\nPlease enter a positive number n > ";
-
-    double n;
-
-    cin >> n;
-
-    cout << "\nHow many times would you like the Babylonian Algorithm to run? The more iterations the more accurate the answer will be. > ";
-
-    int algorithmIterations;
-
-    cin >> algorithmIterations;
-
-    double guess = n / 2;
-
-    for (int i = 0; i < algorithmIterations; i++) {
-
-        double r = n / guess;
-
-        guess = (guess + r) / 2;
-
+    char c0ntinue = 'y';
+    while (c0ntinue == 'y') {
+        cout << "\nPlease enter a positive number n = ";
+        double n;
+        cin >> n;
+        double guess = babylonian_Algorithm(n);
+        cout.setf(ios::fixed);
+        cout.setf(ios::showpoint);
+        cout.precision(2);
+        cout << "\nThe square root of " << n << " is estimated to be " << guess << endl;
+        cout << "\nWould you like to conitnue, y/n? > ";
+        cin >> c0ntinue;
+        c0ntinue = char(tolower(c0ntinue));
+        if (c0ntinue == 'n') {
+            cout << "\nGoodbye!" << endl;
+        }
     }
-
-    cout.setf(ios::fixed);
-    cout.setf(ios::showpoint);
-    cout.precision(2);
-
-    cout << "\nThe square root of " << n << " is estimated to be " << guess << endl;
-
     return 0;
+}
+
+double babylonian_Algorithm(double n) {
+    double guess = n / 2;
+    for (int i = 0; i < 1000; i++) {
+        double r = n / guess;
+        guess = (guess + r) / 2;
+    }
+    return (guess);
 }
