e3d31af2bb90a36ddaa218e17f1a023c1fd310a4
durdyGroBot <jacobWcorbin@gmail.com>
2019-06-12 17:05:03 -0700

    ABSOLUTE CPP Project 610 - WORKING

--- src/Temperature_Class_Exercise.cc
+++ src/Temperature_Class_Exercise.cc
@@ -34,45 +34,72 @@ void Temperature::setTempCelsius(double &degreesCelsius) {
 }
 
 void Temperature::displayTempKelvin() {
-    std::cout << kelvin << " DEGREES KELVIN" << std::endl;
+    std::cout << std::endl << kelvin << " DEGREES KELVIN" << std::endl;
 }
 
 void Temperature::displayTempCelcius() {
     celcius = kelvin - 273.15;
-    std::cout << celcius << "℃" << std::endl;
+    std::cout << std::endl << celcius << "℃" << std::endl;
 }
 
 void Temperature::displayTempFahrenheit() {
     fahrenheit = ((9.0 / 5) * (kelvin - 273.15) + 32);
-    std::cout << fahrenheit << "℉" << std::endl;
+    std::cout << std::endl << fahrenheit << "℉" << std::endl;
 }
 
 int main(int argc, char *argv[]) {
-
-    Temperature temperature;
-    std::cout << std::endl << "What notation is the temperature in? (kelvin/celsius/fahrenheit)  >";
-    std::string notation;
-    std::cin >> notation;
-    for (int i = 0; i < notation.length(); i++) {
-        notation[i] = char(tolower(notation[i]));
-    }
-    std::cout << std::endl << "-------------------------------------------------" << std::endl;
-    std::cout << "Enter the temperature to be converted  > ";
-    double degrees;
-    std::cin >> degrees;
-    while (notation != "kelvin" || notation != "celsius" || notation != "fahrenheit") {
-        std::cout << "That notation is not recognized, please try again  > ";
+    char c0ntinue = 'y';
+    while(c0ntinue == 'y') {
+        Temperature temperature;
+        std::cout << std::endl << "What notation is the temperature in? (kelvin/celsius/fahrenheit)  >";
+        std::string notation;
         std::cin >> notation;
         for (int i = 0; i < notation.length(); i++) {
             notation[i] = char(tolower(notation[i]));
         }
-    }
-    if (notation == "kelvin") {
-        temperature.setTempKelvin(degrees);
-    } else if (notation == "celsius") {
-        temperature.setTempCelsius(degrees);
-    } else {
-        temperature.setTempFahrenheit(degrees);
+        while ((notation != "kelvin") && (notation != "celsius") && (notation != "fahrenheit")) {
+            std::cout << std::endl << "That notation is not recognized, please try again  > ";
+            std::cin >> notation;
+            for (int i = 0; i < notation.length(); i++) {
+                notation[i] = char(tolower(notation[i]));
+            }
+        }
+        std::cout << std::endl << "-------------------------------------------------" << std::endl;
+        std::cout << "Enter the temperature to be converted  > ";
+        double degrees;
+        std::cin >> degrees;
+        if (notation == "kelvin") {
+            temperature.setTempKelvin(degrees);
+        } else if (notation == "celsius") {
+            temperature.setTempCelsius(degrees);
+        } else {
+            temperature.setTempFahrenheit(degrees);
+        }
+        std::cout << std::endl << "What would you like to convert this too? (kelvin/celsius/fahrenheit) > ";
+        std::cin >> notation;
+        for (int i = 0; i < notation.length(); i++) {
+            notation[i] = char(tolower(notation[i]));
+        }
+        while ((notation != "kelvin") && (notation != "celsius") && (notation != "fahrenheit")) {
+            std::cout << std::endl << "That notation is not recognized, please try again  > ";
+            std::cin >> notation;
+            for (int i = 0; i < notation.length(); i++) {
+                notation[i] = char(tolower(notation[i]));
+            }
+        }
+        if (notation == "kelvin") {
+            temperature.displayTempKelvin();
+        } else if (notation == "celsius") {
+            temperature.displayTempCelcius();
+        } else {
+            temperature.displayTempFahrenheit();
+        }
+        std::cout << std::endl << "Would you like to make another conversion? y/n > ";
+        std::cin >> c0ntinue;
+        c0ntinue = char(tolower(c0ntinue));
+        if(c0ntinue == 'n'){
+            std::cout << std::endl << "GoodBye :)" << std::endl;
+        }
     }
 
     return 0;
